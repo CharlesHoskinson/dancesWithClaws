8f08915d4962d3d79ad0a0709f8f013d02c52f1a
Charles Hoskinson <charles.hoskinson@gmail.com>
2026-06-26 17:51:13 -0600

    Allow homepage property in skill frontmatter validation

--- skills/skill-creator/scripts/quick_validate.py
+++ skills/skill-creator/scripts/quick_validate.py
@@ -95,7 +95,7 @@ def validate_skill(skill_path):
                 "Invalid YAML in frontmatter: unsupported syntax without PyYAML installed",
             )
 
-    allowed_properties = {"name", "description", "license", "allowed-tools", "metadata"}
+    allowed_properties = {"name", "description", "license", "allowed-tools", "metadata", "homepage"}
 
     unexpected_keys = set(frontmatter.keys()) - allowed_properties
     if unexpected_keys:
